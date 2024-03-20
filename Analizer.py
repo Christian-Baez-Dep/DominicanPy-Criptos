6c38b239575df8edbefab6a2926a36eb4d38b730
Christian-Baez-Dep <christianbaezcedeno@gmail.com>
2024-03-20 19:35:02 -0400

    Actualizacion del Analizer

--- Analizer.py
+++ Analizer.py
@@ -3,24 +3,23 @@ import os
 import matplotlib.pyplot as plt
 from ExtrDatos import DatosCSV
 class Analizer:
-    extractor = DatosCSV()
-    df_analisis=[]
-    df_volumen_marketcap=[]
-    def analize_volume_n_marketcap(self,lista):
-        df_combined = self.get_dataframe()
+    def __init__(self):
+        self.extractor = DatosCSV()
+        self.df_analisis=[]
+        self.df_volumen_marketcap=[]
+        
+    
+    def analize_volume_n_marketcap(self):
+        df_combined = self.extractor.get_dataframe()
         df_combined['Relacion Volumen Market Cap '] = df_combined['total_volume'] / df_combined['market_cap']
         df_combined_sorted = df_combined.sort_values(by='Relacion Volumen Market Cap ', ascending=False)
         analisis = pd.DataFrame(df_combined_sorted)
-        lista.append(analisis)
+        return analisis
     
     def comportamiento_volumen_capitalizacion(self,lista):
-        df_combined = self.get_dataframe()
+        df_combined = self.extractor.get_dataframe()
         average_volume = df_combined.groupby('coin_name')['total_volume'].mean()
         market_cap = df_combined.groupby('coin_name')['market_cap'].mean()
         volume_marketcap_ratio = average_volume / market_cap
         comportamiento = pd.DataFrame(volume_marketcap_ratio)
-        lista.append(comportamiento)
-        
-    
-    analize_volume_n_marketcap(extractor,df_analisis)
-    comportamiento_volumen_capitalizacion(extractor,df_volumen_marketcap)
+        return comportamiento
